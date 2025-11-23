3b4023da0bc5d9ba2fe3f6deb7002e3c68f3e2f5
jerrywey <jerrywey@qqqq.com>
2025-11-23 17:21:46 +0800

    modify repo's name

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>HELLO, Welcome to Wey's Blogggggg</h1>
+      <h1>HELLO, Welcome to FW's Blogggggg</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
